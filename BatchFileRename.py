990edb76569ec9c058129ef3883f65fa65bfea55
BunchDevelopment <brandon_bunch@icloud.com>
2021-05-19 18:50:49 -0500

    removed useless code

--- BatchFileRename.py
+++ BatchFileRename.py
@@ -1,45 +1,42 @@
 import os
 def main () :
     def rename_file(files, path):
-        rename_type = input("How do you want them renamed? (prefix, suffix, full, quit) \n")
+        rename_type = input('How do you want them renamed? (prefix, suffix, full, quit) \n')
         if(rename_type.lower() == 'prefix'):
-                prefix = input("Prefix: \n")
+                prefix = input('Prefix: \n')
                 for file in files:
                     os.rename(path + file, path + prefix + file)
-                    print("Success!")
 
         elif(rename_type.lower() == 'suffix'):
             suffix = input("Suffix: \n")
             for file in files:
                 split_file = os.path.splitext(file)
                 os.rename(path + file, path + split_file[0] + suffix + split_file[1])
-                print("Success!")
 
         elif(rename_type.lower() == 'full'):
             new_name = input("File Name: \n")
             for ind, file in enumerate(files):
                 split_file = os.path.splitext(file)
                 os.rename(path + file, path + new_name + str(ind) + split_file[1])
-                print("Success!")
 
         elif(rename_type.lower() == 'quit'):
-            print("Thanks fam!")
+            print('Thanks fam!')
 
         else :
-            print('Not a recognized type, try: prefix, suffix, full ')
+            print('Not a recognized type, try: prefix, suffix, full \n')
             rename_file(files, path)
 
     try: 
         path = input('Name a directory that you want the files to be renamed in: \n')
         if(path.endswith('\\') or path.endswith('/')):
             files = os.listdir(path)
-            string_files = " ".join(files)
-            verify_files = input("Are these the files you want renamed? \n " + string_files + '\n')
-            if("y" in verify_files.lower()) :
+            string_files = ' '.join(files)
+            verify_files = input('Are these the files you want renamed? \n ' + string_files + '\n')
+            if('y' in verify_files.lower()) :
                 rename_file(files, path)
-                print('Successfully renamed all your files!')
+                print('Successfully renamed all your files!\n')
             else :
-                print("Not recognized, please try again")
+                print('Not recognized, please try again.\n')
         
         else :
             print('Your path must end with \\ or /')
